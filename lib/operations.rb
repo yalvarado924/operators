e893c965c8e9216f52c32bedf1b5d1bb6802bcd8
Yuliana Alvarado <yalvarado924@gmail.com>
2020-11-10 10:01:31 -0600

    commit tree 4cf6e78afe577d2c507e56063b05e737f8b795f6

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,20 @@
 def unsafe?(speed)
-
+if speed < 40
+    return true
+else
+    false
+end
+if speed > 60
+    return true
+else
+    false
+end
 end
 
 
 
 def not_safe?(speed)
-	
-end
-	
+speed < 40 || speed > 60 ? true : false
+end	
 
 
